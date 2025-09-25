f5197d9998f3559d71e5b400ac1c9eb178712d95
Fabian Frangella <fabianfrangella@gmail.com>
2025-09-25 19:14:51 -0300

    Wip

--- src/components/events/FootballEventDetails.tsx
+++ src/components/events/FootballEventDetails.tsx
@@ -4,6 +4,7 @@ import { formatDate } from "../../utils/events";
 import FootballPitch from "../FootballPitch";
 import MiniMap from "../MiniMap";
 import LocationPickerMap from "../LocationPickerMap";
+import FinishEventButton from "../FinishEventButton";
 
 interface FootballEventDetailsProps {
     event: FootballEvent;
@@ -132,6 +133,12 @@ const FootballEventDetails: React.FC<FootballEventDetailsProps> = ({
                         pitchSize={Number(event.pitchSize)}
                     />
                 </div>
+            </div>
+            <div className="save-changes-container">
+                <FinishEventButton
+                    event={event}
+                    onFinish={() => window.location.reload()}
+                />
             </div>
         </>
     );
