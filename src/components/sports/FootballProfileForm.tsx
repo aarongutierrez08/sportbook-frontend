7d5acf4c392b08ac28bc7ff50a63f57327e25ef0
Fabian Frangella <fabianfrangella@gmail.com>
2025-09-25 22:47:24 -0300

    Mejora estilos

--- src/components/sports/FootballProfileForm.tsx
+++ src/components/sports/FootballProfileForm.tsx
@@ -2,6 +2,7 @@ import { Controller, useFormContext } from "react-hook-form";
 import { Slider, FormControlLabel, Checkbox } from "@mui/material";
 import { FootballPositionSelector } from "./FootballPositionSelector";
 import type { FootballProfileDTO } from "../../types/user";
+import "../../styles/footballProfileForm.css";
 
 export const FootballProfileForm = () => {
   const { control } = useFormContext<FootballProfileDTO>();
@@ -13,32 +14,35 @@ export const FootballProfileForm = () => {
         <FootballPositionSelector />
       </div>
 
-      <Controller
-        name="ability"
-        control={control}
-        render={({ field }) => (
-          <div className="form-group">
-            <label>Nivel (1–10):</label>
-            <Slider {...field} min={1} max={10} valueLabelDisplay="auto" />
-          </div>
-        )}
-      />
+      <div className="profile-controls-container">
+        <Controller
+          name="ability"
+          control={control}
+          render={({ field }) => (
+            <div className="form-group slider-group">
+              <label>Nivel:</label>
+              <Slider {...field} min={1} max={10} valueLabelDisplay="auto" />
+            </div>
+          )}
+        />
 
-      <Controller
-        name="playsOften"
-        control={control}
-        render={({ field }) => (
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={field.value}
-                onChange={(e) => field.onChange(e.target.checked)}
-              />
-            }
-            label="Juego seguido"
-          />
-        )}
-      />
+        <Controller
+          name="playsOften"
+          control={control}
+          render={({ field }) => (
+            <FormControlLabel
+              className="plays-often-checkbox"
+              control={
+                <Checkbox
+                  checked={field.value}
+                  onChange={(e) => field.onChange(e.target.checked)}
+                />
+              }
+              label="Juego seguido"
+            />
+          )}
+        />
+      </div>
     </>
   );
 };
