5c9c80dd2d8f7a631cb8f8aa0b3d913ea1bed118
agutierrez <agutierrez@zetti.com.ar>
2025-10-01 21:28:25 -0300

    fix: delete z-index and add null checks

--- src/components/FootballPitch.tsx
+++ src/components/FootballPitch.tsx
@@ -196,7 +196,7 @@ const FootballPitch: React.FC<FootballPitchProps> = ({
         const player = playerInPosition;
         players.push(
           <div
-            key={player.user.username + position}
+            key={player.user?.username + position}
             className="player"
             draggable
             data-lineup-id={lineup.id}
@@ -273,7 +273,7 @@ const FootballPitch: React.FC<FootballPitchProps> = ({
 
       players.push(
         <div
-          key={player.user.username + 'bench'}
+          key={player.user?.username + 'bench'}
           className="player bench-player"
           draggable
           data-lineup-id={lineup.id}
